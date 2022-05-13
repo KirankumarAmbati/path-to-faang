76c885088e214a0eb76fe9cc9177456ab3cebda8
Kirankumar Ambati <kiran.chinna12520@gmail.com>
2022-05-13 11:30:49 +0530

    :tada: updated bubblesort logic

--- DSA/AlgoExpert-160/Sorting/bubble-sort.js
+++ DSA/AlgoExpert-160/Sorting/bubble-sort.js
@@ -12,7 +12,6 @@ function bubbleSort(array) {
         for (let i = 0; i < array.length; i++) {
             if(array[i] > array[i+1]) {
                 swap(array, i, i+1);
-
                 swapped = true;
             }
         }
