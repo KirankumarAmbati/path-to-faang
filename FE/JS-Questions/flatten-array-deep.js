6b61486c99569b364761114f146bf6e59043c2f7
Kirankumar Ambati <kiran.chinna12520@gmail.com>
2022-05-16 19:41:18 +0530

    :tada: implemented array deep flatten

--- FE/JS-Questions/flatten-array-deep.js
+++ FE/JS-Questions/flatten-array-deep.js
@@ -11,4 +11,4 @@ function deepFlattenArray(array, outputResult = []) {
     return outputResult;
 }
 
-deepFlattenArray([1, [2, 3], [4, [5, 6, [7, 8]]]]); // [1,2,3,4,5,6, 7, 8]
+deepFlattenArray([1, [2, 3], [4, [5, 6, [7, 8]]]]); // [1,2,3,4,5,6,7,8]
